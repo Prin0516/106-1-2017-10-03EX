01f29f5901fde0fcd7dbdc94b72ea8566e655a97
Prin0516 <105021046@live.aisa.edu.tw>
2017-10-03 12:11:34 +0800

    ex

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,10 +5,10 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     private Container cp;
-    private JButton jbtn1=new JButton("Example");
+    private JButton jbtnex=new JButton("Example");
     private JButton jbtn2=new JButton("Encrypt");
-    private JButton jbtn3=new JButton("Clear");
-    private JButton jbtn4=new JButton("Exit");
+    private JButton jbtnclear=new JButton("Clear");
+    private JButton jbtnexit=new JButton("Exit");
     private JLabel jlb=new JLabel("Key");
     private JTextField jtf=new JTextField("0");
     private JPanel jpn=new JPanel(new GridLayout(6,1,3,3));
@@ -30,18 +30,18 @@ public class MainFrame extends JFrame {
         jtaR.setLineWrap(true);
         jtaL.setLineWrap(true);
         jtaR.setEditable(false);
-        jpn.add(jbtn1);
+        jpn.add(jbtnex);
         jpn.add(jbtn2);
         jpn.add(jlb);
         jpn.add(jtf);
-        jpn.add(jbtn3);
-        jpn.add(jbtn4);
+        jpn.add(jbtnclear);
+        jpn.add(jbtnexit);
         cp.add(jspL,BorderLayout.WEST);
         cp.add(jpn,BorderLayout.CENTER);
         cp.add(jspR,BorderLayout.EAST);
         jlb.setHorizontalAlignment(JLabel.CENTER);
 
-        jbtn1.addActionListener(new AbstractAction() {
+        jbtnex.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 jtaL.setText("The Department of Computer Science and Information Engineering, formerly the Department of Information Technology, was established in 2001. The department first offered a Master program only. In 2002, the undergraduate program was established. The Doctoral program was established in 2006. Each year the department admits about 80 undergraduates, 15 graduate students and 3 Ph.D. students.");
@@ -58,7 +58,7 @@ public class MainFrame extends JFrame {
                 jtaR.setText(new String(data));
             }
         });
-        jbtn3.addActionListener(new ActionListener() {
+        jbtnclear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 jtf.setText("0");
@@ -66,7 +66,7 @@ public class MainFrame extends JFrame {
                 jtaR.setText("");
             }
         });
-        jbtn4.addActionListener(new AbstractAction() {
+        jbtnexit.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 System.exit(0);
